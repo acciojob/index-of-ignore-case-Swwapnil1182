021e0239ed55a59b570299a9b24e0347695b61c2
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-04 18:51:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	if(s1.includes(s2)){
+	
 		return s1.toLowerCase().indexOf(s2.toLowerCase())
-	}
-	else{return -1}
-}
+	
+	
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
